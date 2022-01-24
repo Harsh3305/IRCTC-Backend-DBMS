f8267bb080e7c10e30bdd186682be4c89bcf4b34
Harsh3305 <harshverma3305@gmail.com>
2022-01-24 16:00:26 +0530

    Added station routes for user

---
+++ routes/user/Station.js
@@ -0,0 +1,41 @@
+const { createStation, getStation, getAllStation } = require("./../../service/station_service");
+const router = require("express").Router();
+const { verifyUserAccessToken } = require("./../verify_access_token");
+
+router.get("/getStationByID/:station_id", verifyUserAccessToken, async (req, res) => {
+    try {
+        const station_id = req.params.station_id;
+
+
+        getStation(station_id, (error, responce) => {
+            if (error) {
+                res.status(error.error_code).send(error.message);
+            }
+            else {
+                res.status(200).json(responce);
+            }
+        });
+
+    }
+    catch (error) {
+        res.status(500).send("Something went wrong !!");
+    }
+});
+
+
+router.get("/getAllStation", verifyUserAccessToken, async (req, res) => {
+    try {
+        getAllStation((error, responce) => {
+            if (error) {
+                res.status(error.error_code).send(error.message);
+            }
+            else {
+                res.status(200).json(responce);
+            }
+        });
+
+    }
+    catch (error) {
+        res.status(500).send("Something went wrong !!");
+    }
+});
