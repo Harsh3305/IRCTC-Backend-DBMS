2304fa18c4184b14594cda18db511b7811e74762
Harsh3305 <harshverma3305@gmail.com>
2022-01-24 15:41:24 +0530

    Added process_user service

---
+++ service/process_user_service.js
@@ -0,0 +1,102 @@
+const mysql = require("mysql2");
+
+function updateUser(user_id, email, password, username, age, phone, isAdmin, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `UPDATE USER SET EMAIL = '${email}', PASSWORD = '${password}', USERNAME = '${username}', AGE = '${age}', PHONE_NUMBER = '${phone}', IS_ADMIN = '${isAdmin}' WHERE USER_ID = '${user_id}';`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, 'User updated successfully');
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+function deleteUser(user_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `DELETE FROM USER WHERE USER_ID = '${user_id}';`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, 'User deleted successfully');
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+function getUser(user_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM USER WHERE USER_ID = '${user_id}';`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result[0]);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+module.exports = { updateUser, deleteUser, getUser };
