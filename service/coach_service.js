3a87623d1427c40fb56e9705654c256ba1413154
Harsh3305 <harshverma3305@gmail.com>
2022-01-24 15:41:03 +0530

    Added coach service

---
+++ service/coach_service.js
@@ -0,0 +1,105 @@
+const mysql = require("mysql2");
+
+function createCoach(train_id, coach_type, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const values = [train_id, coach_type];
+            const sql_query = `INSERT INTO STATION (TRAIN_ID, COACH_TYPE) VALUES ? `;
+            connection.query(sql_query, [values], (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "Coach created successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+
+function getCoachByID(coach_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM COACH WHERE COACH_ID = '${coach_id}'`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result[0]);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+
+function getCoachesOfTrain(train_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM COACH WHERE TRAIN_ID = '${train_id}'`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result);
+                }
+            })
+
+            connection.end((error) => {
+                if (error) {
+                    console.error(error);
+                }
+            });
+        }
+    });
+}
+
+module.exports = { createCoach, getCoachByID, getCoachesOfTrain }
