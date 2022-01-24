411a78e038acd94c6fcd2bb4a94bd31145d1ebc9
Harsh3305 <harshverma3305@gmail.com>
2022-01-24 15:51:52 +0530

    Added create station route

--- routes/admin/Station.js
+++ routes/admin/Station.js
@@ -1,7 +1,28 @@
-const { getStation, getAllStation } = require("./../../service/station_service");
+const { createStation, getStation, getAllStation } = require("./../../service/station_service");
 const router = require("express").Router();
 const { verifyAdminAccessToken } = require("./../verify_access_token");
 
+router.post("/createStation", verifyAdminAccessToken, async (req, res) => {
+    try {
+
+        const station_name = req.body.station_name;
+        const type = req.body.type;
+        const station_code = req.body.station_code;
+
+        createStation(station_name, type, station_code, (error, responce) => {
+            if (error) {
+                res.status(error.error_code).send(error.message);
+            }
+            else {
+                res.status(200).json(responce);
+            }
+        });
+
+    }
+    catch (error) {
+        res.status(500).send("Something went wrong !!");
+    }
+});
 
 
 router.get("/getStationByID/:station_id", verifyAdminAccessToken, async (req, res) => {
