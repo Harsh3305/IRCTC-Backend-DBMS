3822b303f29112ce1058e2c4096bc80831ab7e86
Harsh3305 <harshverma3305@gmail.com>
2022-01-25 19:29:10 +0530

    Added Ticket_cluster service

---
+++ service/ticket.js
@@ -0,0 +1,169 @@
+const mysql = require("mysql2");
+
+
+function creatTicket(ticket_cluster_id, passanger_id, seat_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const value = [ticket_cluster_id, passanger_id, seat_id];
+            const sql_query = `INSERT INTO TICKER (TICKER_CLUSTER_ID, PASSANGER_ID, SEAT_ID) VALUES = ?;`;
+            connection.query(sql_query, [value], (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, 'Ticket created successfully');
+                }
+            });
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+
+function updateTicket(ticket_id, ticket_cluster_id, passanger_id, seat_id, callback) {
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `UPDATE TICKER SET TICKET_ID = '${ticket_id}, TICKET_CLUSTER_ID = '${ticket_cluster_id}, PASSENGER_ID = '${passanger_id}, SEAT_ID = ${seat_id}';`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "Ticket updated successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+
+function deleteTicket(ticket_id, callback) {
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `DROP FROM TICKET WHERE TICKET_ID = '${ticket_id};`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "Ticket deleted successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+function getTicket(ticket_id, callback) {
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM PASSANGER WHERE TICKET_ID = '${ticket_id}';`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result[0]);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+function getAllTicket(callback) {
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM TICKET;`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    })
+}
+
+
+module.exports = { creatTicket, updateTicket, deleteTicket, getTicket, getAllTicket };
---
+++ service/ticket_cluster.js
@@ -0,0 +1,170 @@
+const mysql = require("mysql2");
+
+function createTicketCluster(train_departure_time, source_id, destination_id, payment_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const values = [train_departure_time, source_id, destination_id, payment_id];
+            const sql_query = `INSERT INTO TICKET_CLUSTER (TRAIN_DEPARTURE_TIME, SOURCE_ID, DESTINATION_ID, PAYMENT_ID) VALUES ? `;
+            connection.query(sql_query, [values], (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "TICKET_CLUSTER created successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+function updateTicketCluster(train_departure_time, source_id, destination_id, payment_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const values = [train_departure_time, source_id, destination_id, payment_id];
+            const sql_query = `UPDATE TICKET_CLUSTER SET TRAIN_DEPARTURE_TIME = '${train_departure_time}', SOURCE_ID = '${source_id}, DESTINATION_ID = '${destination_id}, PAYMENT_ID = '${payment_id} ;`;
+            connection.query(sql_query, [values], (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "TICKET_CLUSTER updated successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+function deleteTicketCluster(ticket_cluster_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const values = [train_departure_time, source_id, destination_id, payment_id];
+            const sql_query = `DROP TICKET_CLUSTER WHERE TICKET_CLUSTER_ID = '${ticket_cluster_id}';`;
+            connection.query(sql_query, [values], (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, "TICKET_CLUSTER updated successfully");
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+function getTicketCluster(ticket_cluster_id, callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM TICKET_CLUSTER WHERE TICKER_CLUSTER_ID = '${ticket_cluster_id};`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result[0]);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+
+function getAllTicketCluster(callback) {
+
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+
+    connection.connect((error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            const sql_query = `SELECT * FROM TICKET_CLUSTER;`;
+            connection.query(sql_query, (error, result) => {
+                if (error) {
+                    callback({ error_code: 500, error_message: error.message });
+                }
+                else {
+                    callback(null, result[0]);
+                }
+            })
+        }
+
+        connection.end((error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
+    });
+}
+module.exports = { createTicketCluster, updateTicketCluster, deleteTicketCluster, getAllTicketCluster, getTicketCluster };
