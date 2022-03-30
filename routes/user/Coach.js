be128ab7e389e41631a2a47c03f20ca0d11db4c5
Harsh3305 <harshverma3305@gmail.com>
2022-03-30 18:47:49 +0530

    Minor update in get coaches of train

--- routes/user/Coach.js
+++ routes/user/Coach.js
@@ -1,6 +1,7 @@
 const { getCoachByID, getCoachesOfTrain } = require("./../../service/coach_service");
 const router = require("express").Router();
 const { verifyUserAccessToken } = require("./../verify_access_token");
+const { getAllVacentSeatOfCoach } = require("./../../service/seat_service");
 
 router.get("/getCoachByID/:coach_id", verifyUserAccessToken, async (req, res) => {
     try {
@@ -24,13 +25,42 @@ router.get("/getCoachesByTrainID/:train_id", verifyUserAccessToken, async (req, 
     try {
         const train_id = req.params.train_id;
 
-
         getCoachesOfTrain(train_id, (error, responce) => {
             if (error) {
                 res.status(error.error_code).send(error.error_message);
             }
             else {
-                res.status(200).json(responce);
+                for (let i = 0; i < responce.length; i++) {
+                    const coach_id = responce[i].COACH_ID
+                    try {
+                        getAllVacentSeatOfCoach(coach_id, (error, message) => {
+                            if (error) {
+                                res.status(error.error_code).send(error.error_message)
+                            }
+                            else {
+                                try {
+
+                                    responce[i].seat_available = message.length;
+                                    if (i === responce.length - 1) {
+                                        res.status(200).send(responce);
+                                        return;
+                                    }
+                                }
+                                catch (error) {
+                                    responce[i].seat_available = 0;
+                                    if (i === responce.length - 1) {
+                                        res.status(200).send(responce);
+                                        return;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    catch (error) {
+                        res.status(200).send(0);
+                        return;
+                    }
+                }
             }
         });
 
