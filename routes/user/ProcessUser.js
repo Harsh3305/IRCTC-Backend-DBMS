5930ed62e15836189d5ab934484c1f71e73ecdf0
souravb786 <souravbaghel786@gmail.com>
2022-04-24 14:12:33 +0530

    final code

--- routes/user/ProcessUser.js
+++ routes/user/ProcessUser.js
@@ -1,69 +1,76 @@
 const express = require("express");
 const router = express.Router();
 const { verifyUserAccessToken } = require("./../verify_access_token");
-const { deleteUser, getUser, updateUser } = require("./../../service/process_user_service");
+const {
+  deleteUser,
+  getUser,
+  updateUser,
+} = require("./../../service/process_user_service");
 router.put("/updateUser", verifyUserAccessToken, async (req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-        const username = req.body.username;
-        const age = req.body.age;
-        const phone = req.body.phone;
-        const user_id = req.headers.id
-        // update up user and send access token
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    const age = req.body.age;
+    const phone = req.body.phone;
+    const user_id = req.headers.id;
+    // update up user and send access token
 
-        updateUser(user_id, email, password, username, age, phone, 0, (error, responce) => {
-            if (error) {
-                res.status(error.error_code).send(error.error_message);
-            }
-            else {
-                res.status(200).json(responce);
-            }
-        });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send("Something went wrong !!")
-    }
+    updateUser(
+      user_id,
+      email,
+      password,
+      username,
+      age,
+      phone,
+      0,
+      (error, responce) => {
+        if (error) {
+          res.status(error.error_code).send(error.error_message);
+        } else {
+          res.status(200).json(responce);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong !!");
+  }
 });
 
 router.delete("/deleteUser", verifyUserAccessToken, async (req, res) => {
-    try {
-        const user_id = req.params.id;
-        // Delete User
+  try {
+    const user_id = req.header.id;
+    // Delete User
 
-        deleteUser(user_id, (error, responce) => {
-            if (error) {
-                res.status(error.error_code).send(error.error_message);
-            }
-            else {
-                res.status(200).json(responce);
-            }
-        });
-    }
-    catch (error) {
-        res.status(500).send("Something went wrong !!");
-    }
+    deleteUser(user_id, (error, responce) => {
+      if (error) {
+        res.status(error.error_code).send(error.error_message);
+      } else {
+        res.status(200).json(responce);
+      }
+    });
+  } catch (error) {
+    res.status(500).send("Something went wrong !!");
+  }
 });
 
 router.get("/getUser", verifyUserAccessToken, async (req, res) => {
-    try {
-        const user_id = req.params.id;
-        // Get User by id
+  try {
+    const user_id = req.headers.id;
+    // Get User by id
 
-        getUser(user_id, (error, responce) => {
-            if (error) {
-                res.status(error.error_code).send(error.error_message);
-            }
-            else {
-                res.status(200).json(responce);
-            }
-        });
-    }
-    catch (error) {
-        res.status(500).send("Something went wrong !!");
-    }
+    getUser(user_id, (error, responce) => {
+      console.log(responce);
+      if (error) {
+        res.status(error.error_code).send(error.error_message);
+      } else {
+        res.status(200).json(responce);
+      }
+    });
+  } catch (error) {
+    res.status(500).send("Something went wrong !!");
+  }
 });
-
 
 module.exports = router;
